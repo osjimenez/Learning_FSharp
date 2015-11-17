53e1d7614a3e9a5a2e74b69d2ee97947c5a0011a
Oscar Jimenez <social@ittxa.com>
2015-11-17 16:03:17 +0100

    .

---
+++ src/ConsoleApplication/GettingStarted.fsx
@@ -0,0 +1,8 @@
+﻿namespace Namespace1.Library1
+    module Module2 =
+        let internal Version() =
+            "Version 1.0"
+
+namespace Namespace1.Library2
+    module Module1 =
+        let x = Namespace1.Library1.Module2.Version()
